d4443e20219adbae199422c45a7753e75e24bf0b
Smirnov2av <adt963256@gmail.com>
2024-11-01 19:12:38 +0300

    Add files via upload

---
+++ Финансовая подушка безопасности.py
@@ -0,0 +1,10 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+i = 0
+while spend < money_capital + salary:
+    money_capital -= spend-salary
+    spend *= 1.05
+    i += 1
+print("Количество месяцев, которое можно протянуть без долгов:", i)
